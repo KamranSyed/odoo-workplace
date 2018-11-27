1d6c41e862dfb0758971300aa9a7b44a1da15a3f
Kamran Syed <lovelykamran@gmail.com>
2018-11-27 13:46:25 +0500

    name field added for unification of all child classes of WorkplaceResource.

--- models/resHuman.py
+++ models/resHuman.py
@@ -8,5 +8,6 @@ class ResourceHuman(models.Model):
 	_inherits 	= 'ksworkplace.resource'
 
 	job			= fields.Many2one('hr.job', string="Job Title", required=True)
+	name		= fields.Char()(related='job.name')
 	resId 		= fields.Many2one('ksworkplace.resource', string="Resource Id", ondelete='cascade', required=True, auto_join=True)
 
